7273710977271480e2fce6442c8bf4e014e58840
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-09-10 15:08:01 +0800

    alvistack/v3.22.0
        git clean -xdf
        go mod download
        go mod vendor
        tar zcvf ../helm_3.22.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp helm.spec ../helm_3.22.0-1.spec
        cp ../helm*3.22.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/helm-helm-3.22.0/
        rm -rf ../helm*3.22.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- pkg/helmpath/lazypath_unix.go
+++ pkg/helmpath/lazypath_unix.go
@@ -15,17 +15,11 @@
 
 package helmpath
 
-import (
-	"path/filepath"
-
-	"k8s.io/client-go/util/homedir"
-)
-
 // dataHome defines the base directory relative to which user specific data files should be stored.
 //
 // If $XDG_DATA_HOME is either not set or empty, a default equal to $HOME/.local/share is used.
 func dataHome() string {
-	return filepath.Join(homedir.HomeDir(), ".local", "share")
+	return "/usr/share"
 }
 
 // configHome defines the base directory relative to which user specific configuration files should
@@ -33,7 +27,7 @@ func dataHome() string {
 //
 // If $XDG_CONFIG_HOME is either not set or empty, a default equal to $HOME/.config is used.
 func configHome() string {
-	return filepath.Join(homedir.HomeDir(), ".config")
+	return "/etc"
 }
 
 // cacheHome defines the base directory relative to which user specific non-essential data files
@@ -41,5 +35,5 @@ func configHome() string {
 //
 // If $XDG_CACHE_HOME is either not set or empty, a default equal to $HOME/.cache is used.
 func cacheHome() string {
-	return filepath.Join(homedir.HomeDir(), ".cache")
+	return "/var/cache"
 }
